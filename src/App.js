f7378f7b55b0f9b028ce1755d2485469785a85e9
Christopher Cardoso <cardoso.christopher01@gmail.com>
2019-02-26 22:33:51 +0100

    Adding the filter function on the search engine

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 import CardList from "./CardList";
 
-const app = () => {
-  return (
-    <div>
-      <h1>RoboFriends</h1>
-      <SearchBox />
-      <CardList robots={robots} />
-    </div>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: ""
+    };
+  }
+  onSearchChange = event => {
+    this.setState({ searchfield: event.target.value });
+  };
 
-export default app;
+  render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div>
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
+
+export default App;
