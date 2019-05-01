eaf4fac11c7233bdcc12496ea595552c51f40c3e
Christopher Cardoso <cardoso.christopher01@gmail.com>
2019-05-01 22:31:36 +0200

    Updates

---
+++ precache-manifest.bcf77b355fd5fd229a2d786a12617dd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8780a84d519538b53511",
+    "url": "/robofriends/static/css/main.3f651ed9.chunk.css"
+  },
+  {
+    "revision": "8780a84d519538b53511",
+    "url": "/robofriends/static/js/main.8780a84d.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.143eb19a.js"
+  },
+  {
+    "revision": "97a321e1e803198be9e4",
+    "url": "/robofriends/static/css/2.49079db8.chunk.css"
+  },
+  {
+    "revision": "97a321e1e803198be9e4",
+    "url": "/robofriends/static/js/2.97a321e1.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "81fbec9fce5c013da3eda8fefa1e1c97",
+    "url": "/robofriends/index.html"
+  }
+];
